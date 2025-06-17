e86fd6604d1005037cb2f677c92d3a066b29c205
Diala Wilson <diala.i.wilson@gmail.com>
2025-06-17 18:11:17 +0100

    register done

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,38 @@
 "use client";
 import Navbar from "@/components/Navbar";
-import { Box, Flex, Text, Stack,Field, Input,HStack, useBreakpointValue, FieldRoot } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Stack,
+  Field,
+  Input,
+  useBreakpointValue,
+  FieldRequiredIndicator,
+  Portal,
+  Select,
+  createListCollection,
+  Button,
+} from "@chakra-ui/react";
+// import { Select } from "@ark-ui/react";
 import Image from "next/image";
-import FrontPhoto from "@/asset/img/training.jpg";
+import RegPhoto from "@/asset/img/training.jpg";
+import FrontPhoto from "../../asset/img/building.jpg";
 
 
 export default function Register() {
   const isMobile = useBreakpointValue({ base: true, md: false });
+
+  const frameworks = createListCollection({
+    items: [
+      { label: "1 Month", value: "1 month" },
+      { label: "2 Months", value: "2 months" },
+      { label: "3 Months", value: "3 months" },
+      { label: "6 Months", value: "6 months" },
+      { label: "12 Months", value: "12 months" },
+    ],
+  });
+
   return (
     <>
       <Box minH="100vh" overflow="hidden">
@@ -16,18 +42,21 @@ export default function Register() {
             <Box
               position="relative"
               w="50%"
-              h="100%"
+              h="89vh"
               overflow="hidden"
+               border="2px solid red"
               bgGradient="linear(to-r, green.100, green.300"
             >
               <Image
-                src={FrontPhoto}
-                alt="MR-Soft Building"
-                fill
-                style={{ objectFit: "cover" }}
+              src={FrontPhoto}
+              // src="../../asset/img/training.jpg"
+              alt="MR-Soft Building"
+              fill
+              style={{ objectFit: "cover" }}
+              priority
               />
               <Box
-                position="absolute"
+              position="absolute"
                 bottom={0}
                 w="100%"
                 h="70px"
@@ -36,7 +65,6 @@ export default function Register() {
             </Box>
           )}
 
-          {/* register section */}
           <Flex
             align="center"
             justify="center"
@@ -52,30 +80,100 @@ export default function Register() {
               >
                 IT Students Registration Form
               </Text>
-              <Flex gap={10} w="full" direction={{base:"column", md:"row"}}>
+
+              <Flex gap={10} w="full" direction={{ base: "column", md: "row" }}>
                 <Field.Root required>
                   <Field.Label>
-                    Name <Field.RequiredIndicator/>
-
-
+                    Name <Field.RequiredIndicator />
                   </Field.Label>
-                  <Input variant="flushed" placeholder="Name:"/>
+                  <Input variant="flushed" placeholder="Name:" />
                 </Field.Root>
                 <Field.Root required>
                   <Field.Label>
-                    Surname <Field.RequiredIndicator/>
+                    Surname <Field.RequiredIndicator />
                   </Field.Label>
-                  <Input variant="flushed" placeholder="Surname:"/>
+                  <Input variant="flushed" placeholder="Surname:" />
                 </Field.Root>
               </Flex>
-              <Stack>
-                <Field.Root required>
-                <Field.Label>
 
+              <Field.Root required>
+                <Field.Label>
+                  Institution <FieldRequiredIndicator />
                 </Field.Label>
+                <Input variant="flushed" placeholder="Institution" />
+              </Field.Root>
+
+              <Flex gap={10}>
+                <Field.Root required>
+                  <Field.Label>
+                    Matriculation Number <FieldRequiredIndicator />
+                  </Field.Label>
+                  <Input variant="flushed" placeholder="Mat Number" />
                 </Field.Root>
 
-            </Stack>
+                <Field.Root>
+                  <Select.Root
+                    // key={variant}
+                    collection={frameworks}
+                  >
+                    <Select.HiddenSelect />
+                    <Select.Label>Duration of Internship </Select.Label>
+                    <Select.Control>
+                      <Select.Trigger>
+                        <Select.ValueText placeholder="0 Months" />
+                      </Select.Trigger>
+                      <Select.IndicatorGroup>
+                        <Select.Indicator />
+                      </Select.IndicatorGroup>
+                    </Select.Control>
+                    <Portal>
+                      <Select.Positioner>
+                        <Select.Content variant="flushed">
+                          {frameworks.items.map((framework) => (
+                            <Select.Item item={framework} key={framework.value}>
+                              {framework.label}
+                              <Select.ItemIndicator />
+                            </Select.Item>
+                          ))}
+                        </Select.Content>
+                      </Select.Positioner>
+                    </Portal>
+                  </Select.Root>
+                </Field.Root>
+              </Flex>
+
+              <Flex>
+                <Field.Root required>
+                  <Field.Label>
+                    Course of study <FieldRequiredIndicator />
+                  </Field.Label>
+                  <Input variant="flushed" placeholder="Course of study" />
+                </Field.Root>
+                <Field.Root required>
+                  <Field.Label>
+                    Year of course <FieldRequiredIndicator />
+                  </Field.Label>
+                  <Input variant="flushed" placeholder="Year of course" />
+                </Field.Root>
+              </Flex>
+              <Flex gap={4} mt={4}>
+                <Button
+                  colorScheme="green"
+                  flex={1}
+                  onClick={() => (window.location.href = "/dashboard")}
+                >
+                  Register
+                </Button>
+                <Button
+                  variant="outline"
+                  colorScheme="green"
+                  flex={1}
+                  onClick={() => (window.location.href = "/")}
+                >
+                  Login
+                </Button>
+              </Flex>
+              {/* closing stack */}
             </Stack>
           </Flex>
         </Flex>
